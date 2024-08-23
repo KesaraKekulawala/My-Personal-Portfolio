39c791c5f9ed2201af593551f075729f5df432ba
kesara <udeshan521@gmail.com>
2024-08-24 00:40:47 +0530

    Create Navigation Bar

---
+++ src/components/NavBar.js
@@ -0,0 +1,66 @@
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import logo from '../assets/img/welcome.png';
+import navIcon1 from '../assets/img/linkedin-round-svgrepo-com.svg';
+import navIcon2 from '../assets/img/facebook-svgrepo-com.svg';
+import navIcon3 from '../assets/img/github-142-svgrepo-com.svg';
+import { HashLink } from 'react-router-hash-link';
+import {
+    BrowserRouter as Router
+} from "react-router-dom";
+
+export const NavBar = () => {
+    const [ activeLink, setActiveLink ] = useState('home');
+    const [ scrolled, setScrolled ] = useState(false);
+
+    useEffect(() => {
+        const onScroll = () => {
+            if (window.scrollY > 50) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        }
+
+        window.addEventListener('scroll', onScroll);
+
+        return () => window.removeEventListener('scroll', onScroll);
+    }, [])
+
+    const onUpdateActiveLink = (value) => {
+        setActiveLink(value);
+    }
+
+    return (
+        <Router>
+            <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+                <Container>
+                    <Navbar.Brand href="/" className='BrandName'>
+                        {/* <img src={logo} alt="Logo" /> */}
+                        <h1> WELCOME ! </h1>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav">
+                        <span className="navbar-toggler-icon"></span>
+                    </Navbar.Toggle>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+                            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+                            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+                        </Nav>
+                        <span className='navbar-text'>
+                            <div className='social-icon'>
+                                <a href='https://www.linkedin.com/in/kesara-kekulawala-392077230?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app'><img src={navIcon1} alt='LinkedIn' /></a>
+                                <a href='https://www.facebook.com/kesara.kekulawala.1?mibextid=ZbWKwL'><img src={navIcon2} alt='Facebook' /></a>
+                                <a href='https://github.com/KesaraKekulawala'><img src={navIcon3} alt='Github' /></a>
+                            </div>
+                            <HashLink to='#connect'>
+                                <button className='vvd'><span>Let's Connect</span></button>
+                            </HashLink>
+                        </span>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </Router>
+    );
+}
