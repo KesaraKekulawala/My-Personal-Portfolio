aad2f5d12cf0d92a359a832dfa1945fdc6b233d1
kesara <udeshan521@gmail.com>
2024-08-25 14:13:38 +0530

    Create banner

---
+++ src/components/Banner.js
@@ -0,0 +1,58 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import { useState,useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.svg";
+export const Banner = () => {
+    const [loopNum, setLoopNum] = useState(0);
+    const[isDeleting, setIsDeleting] = useState (false);
+    const toRotate =["Web Developer", "Web Designer","UI/UX Designer","Mobile Application Developer"];
+    const [text,setText] = useState('');
+    const [delta, setDelta] = useState(300 - Math.random()*100);
+    const period = 2000;
+
+    useEffect(() => {
+        let ticker = setInterval(() =>{
+            tick();
+
+        },delta)
+        return () => {clearInterval(ticker)};
+    }, [text])
+
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0,text.length -1) : fullText.substring(0, text.length +1);
+        setText(updatedText);
+        if(isDeleting){
+            setDelta(prevDelta => prevDelta/2)
+        }
+        if(!isDeleting && updatedText === fullText){
+            setIsDeleting(true);
+            setDelta(period);
+
+        }else if(isDeleting && updatedText === ''){
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setDelta(500);
+        }
+    }
+    return (
+        <section className="banner" id="home">
+            <Container>
+                <Row className="align-items-center">
+                    <Col xs={12} md={6} xl={7} >
+                    <span ClassName = "tagline"> Welcome to my Portfolio</span>
+                    <h1>{"Hi I'm Kesara, "}<span classname="wrap">{text}</span></h1>
+                    <p>I am a third-year undergraduate at the Faculty of Information Technology, University of Moratuwa, with a strong passion for technology and design. My expertise spans Mobile App Development, Full Stack Web Development, UI/UX Design, and Graphic Design. I thrive on creating innovative solutions that blend functionality with aesthetics. My portfolio showcases a diverse range of projects where I've applied my skills to deliver high-quality, user-centered experiences. I'm committed to continuous learning and look forward to contributing to impactful projects in the tech industry.</p>
+                    <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25}/></button>
+                    </Col>
+                    <Col xs={12} md={6} xl={5} >
+                    <img src={headerImg} alt="Header Image"/>
+                    </Col>
+                </Row>
+            </Container>
+
+        </section>
+    )
+}
