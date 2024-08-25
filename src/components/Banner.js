758d2ce697d777095f858832ce86438a38db43fa
kesara <udeshan521@gmail.com>
2024-08-25 21:20:18 +0530

    Create Skill and Project sections

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,8 +1,11 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import { useState,useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 import headerImg from "../assets/img/header-img.svg";
+import 'animate.css';
+import TrackVisibility from "react-on-screen";
 export const Banner = () => {
     const [loopNum, setLoopNum] = useState(0);
     const[isDeleting, setIsDeleting] = useState (false);
@@ -41,14 +44,25 @@ export const Banner = () => {
         <section className="banner" id="home">
             <Container>
                 <Row className="align-items-center">
-                    <Col xs={12} md={6} xl={7} >
-                    <span ClassName = "tagline"> Welcome to my Portfolio</span>
-                    <h1>{"Hi I'm Kesara, "}<span classname="wrap">{text}</span></h1>
-                    <p>I am a third-year undergraduate at the Faculty of Information Technology, University of Moratuwa, with a strong passion for technology and design. My expertise spans Mobile App Development, Full Stack Web Development, UI/UX Design, and Graphic Design. I thrive on creating innovative solutions that blend functionality with aesthetics. My portfolio showcases a diverse range of projects where I've applied my skills to deliver high-quality, user-centered experiences. I'm committed to continuous learning and look forward to contributing to impactful projects in the tech industry.</p>
-                    <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25}/></button>
+                    <Col xs={12} md={6} xl={7}>
+                        <TrackVisibility>
+                            {({ isVisible }) =>
+                                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                                    <span className="tagline">Welcome to my Portfolio</span>
+                                    <h1>{`Hi I'm Kesara!  `}<span className="wrap">{text}</span></h1>
+                                    <p>I am a third-year undergraduate at the Faculty of Information Technology, University of Moratuwa, with a strong passion for technology and design. My expertise spans Mobile App Development, Full Stack Web Development, UI/UX Design, and Graphic Design. I thrive on creating innovative solutions that blend functionality with aesthetics. My portfolio showcases a diverse range of projects where I've applied my skills to deliver high-quality, user-centered experiences. I'm committed to continuous learning and look forward to contributing to impactful projects in the tech industry.</p>
+                                    <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25} /></button>
+                                </div>
+                            }
+                        </TrackVisibility>
                     </Col>
-                    <Col xs={12} md={6} xl={5} >
-                    <img src={headerImg} alt="Header Image"/>
+                    <Col xs={12} md={6} xl={5}>
+                        <TrackVisibility>
+                            {({ isVisible }) =>
+                                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                                    <img src={headerImg} alt="Header Img" />
+                                </div>}
+                        </TrackVisibility>
                     </Col>
                 </Row>
             </Container>
---
+++ src/components/Projects.js
@@ -0,0 +1,99 @@
+import { Col, Container, Nav, Row, Tab } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+export const Projects = () => {
+    const projects = [
+        {
+            title: "Gesture control robot car with mechanical arm for an entertaining game (Group)",
+            description: "First - Year Hardware Group Project Of my university. We develop a robot car and a mechanical arm was designed for an entertaining game. Car hand were controlled by gesture hand. Arduino used to make the game",
+            imgUrl: projImg1,
+        },
+        {
+            title: "Salon Appointment And Scheduling  System - StyleSync (Group)",
+            description: "Second - Year Software Project, A System Includes Mobile App and two Web Apps for Salon owner, Customers and  System Admin to Scheduling/Managing Appointments.",
+            imgUrl: projImg2,
+        },
+        {
+            title: "Personal Portfolio (Individual)",
+            description: "A responsive web application using React and Bootstrap",
+            imgUrl: projImg3,
+        },
+
+    ];
+    return (
+        <section className="project" id="project">
+            <Container>
+                <Row>
+                    <Col>
+                    <h2>Projects</h2>
+                    <p>These Are my Individual and Group Projects about what I did in Past </p>
+                    <Tab.Container id="projects-tabs" defaultActivityKey="first">
+                    <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab"  >
+                    <Nav.Item>
+                        <Nav.Link eventKey="first">Tab One</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="second">Tab Two</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="third">Tab Third</Nav.Link>
+                    </Nav.Item>
+                    </Nav>
+                    
+                    <Tab.Content>
+                        <Tab.Pane eventKey="first">
+                            <Row>
+                                {
+                                    projects.map((project,index) =>{
+                                        return (
+                                            <ProjectCard
+                                            key={index}
+                                            {...project}
+                                            />
+                                        )
+                                    })
+                                }
+                            </Row>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="second">
+                        <Row>
+                                {
+                                    projects.map((project,index) =>{
+                                        return (
+                                            <ProjectCard
+                                            key={index}
+                                            {...project}
+                                            />
+                                        )
+                                    })
+                                }
+                            </Row>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="third">
+                        <Row>
+                                {
+                                    projects.map((project,index) =>{
+                                        return (
+                                            <ProjectCard
+                                            key={index}
+                                            {...project}
+                                            />
+                                        )
+                                    })
+                                }
+                            </Row>
+                        </Tab.Pane>
+                    </Tab.Content>
+                    </Tab.Container>
+                    </Col>
+                </Row>
+            </Container>
+            <img className="background-image-right" src={colorSharp2} alt="bg"></img>
+        </section>
+
+
+    )
+}
